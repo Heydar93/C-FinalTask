798e69d846596b97b654d475abe1a0ea38738697
ASUS <ASUS@DESKTOP-55F15SS>
2023-05-14 16:37:05 +0400

    Required features added successfully.

---
+++ CA-test/TaskManagement/Client/Commands/AddCommentToBlog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Common.Commands;
+using TaskManagement.Contants;
+using TaskManagement.Database.Models;
+using TaskManagement.Database.Repositories;
+
+namespace TaskManagement.Client.Commands
+{
+    public class AddCommentToBlog : ICommandHandler
+    {
+        public void Handle()
+        {
+            Console.WriteLine("Enter the blog code:");
+            string blogCode = Console.ReadLine();
+
+            Blog blog = BlogRepository.GetByCode(blogCode);
+
+            if (blog == null)
+            {
+                Console.WriteLine("Invalid blog code");
+                return;
+            }
+
+            Console.WriteLine("Enter the comment text:");
+            string commentText = Console.ReadLine();
+
+            var comment = new Comment(commentText, blog.Owner);
+            blog.Comments.Add(comment);
+
+            Console.WriteLine("Comment added successfully");
+
+            string message = $"{blogCode} blog code {comment.Owner.Name} {comment.Owner.LastName} tərəfindən comment əlavə olundu.";
+            Console.WriteLine(message);
+        }
+
+        public class Comment
+        {
+            public string Text { get; set; }
+            public User Owner { get; set; }
+
+            public Comment(string text, User owner)
+            {
+                Text = text;
+                Owner = owner;
+            }
+        }
+    }
+}
+
+
+
---
+++ CA-test/TaskManagement/Jsons/Json.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.Json;
+using TaskManagement.Database;
+using TaskManagement.Database.Models;
+
+namespace TaskManagement.Jsons
+{
+    public  class Json
+    {
+        public static async Task SerializePersonToFile()
+        {
+            var fileName = "Users.json";
+            var myList = DataContext.Users;
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            using var stream = new FileStream(fileName, FileMode.Append);
+            await JsonSerializer.SerializeAsync(stream, myList, options);
+            await stream.DisposeAsync();
+        }
+        public static async Task SerializeMessageToFile()
+        {
+            var fileName = "Message.json";
+            var myList = DataContext.Messages;
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            using var stream = new FileStream(fileName, FileMode.Append);
+            await JsonSerializer.SerializeAsync(stream, myList, options);
+            await stream.DisposeAsync();
+        }
+        public static async Task SerializeBlogToFile()
+        {
+            var fileName = "Blog.json";
+            var myList = DataContext.Blogs;
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            using var stream = new FileStream(fileName, FileMode.Append);
+            await JsonSerializer.SerializeAsync(stream, myList, options);
+            await stream.DisposeAsync();
+        }
+
+        public static async Task<List<User>> DeserializePersonFromFile()
+        {
+            //var fileName = "Users.json";
+
+            //using var stream = new FileStream(fileName, FileMode.Open);
+            //var people = await JsonSerializer.DeserializeAsync<List<User>>(stream);
+
+            //return people;
+            var fileName = "Users.json";
+            var people = Newtonsoft.Json.JsonConvert.DeserializeObject<List<User>>(fileName);
+            //using var stream = new FileStream(fileName, FileMode.Open);
+            //var options = new JsonSerializerOptions
+            //{
+            //    PropertyNameCaseInsensitive = true
+            //};
+            //var people = await JsonSerializer.DeserializeAsync<List<User>>(stream, options);
+
+            return people;
+        }
+        public static async Task<List<Message>> DeserializeMessageFromFile()
+        {
+            var fileName = "Message.json";
+
+            using var stream = new FileStream(fileName, FileMode.Open);
+            var messages = await JsonSerializer.DeserializeAsync<List<Message>>(stream);
+
+            return messages;
+        }
+        public static async Task<List<Blog>> DeserializeBlogFromFile()
+        {
+            var fileName = "Blog.json";
+
+            using var stream = new FileStream(fileName, FileMode.OpenOrCreate);
+            var blogs = await JsonSerializer.DeserializeAsync<List<Blog>>(stream);
+
+            return blogs;
+        }
+
+    }
+}
